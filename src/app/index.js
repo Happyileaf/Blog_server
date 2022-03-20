1909d0e9449d82596c612949a56aeb0515203f57
happy <997401767@qq.com>
2022-03-20 16:11:03 +0800

    引入koa2-cors模块，处理跨域问题

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-15 16:02:26
- * @LastEditTime: 2022-03-18 17:31:44
+ * @LastEditTime: 2022-03-20 15:20:38
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \Blog_server\src\app\index.js
@@ -13,6 +13,9 @@ const Koa = require('koa')
 const KoaBody = require('koa-body')
 const KoaStatic = require('koa-static')
 const parameter = require('koa-parameter')
+
+const cors = require('koa2-cors');
+
 
 const errHandler = require('./errHandler')
 const router = require('../router')
@@ -34,7 +37,7 @@ app.use(
 
 app.use(KoaStatic(path.join(__dirname, '../public/upload')))
 app.use(parameter(app))
-
+app.use(cors());
 app.use(router.routes()).use(router.allowedMethods())
 
 // 统一的错误处理
